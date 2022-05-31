9a1f9f79aa1e25f5bba773c683e6348fdfa89059
Airat <example@gmail.com>
2022-05-31 08:30:31 +0300

    first commit

---
+++ jsstart.js
@@ -0,0 +1,11 @@
+// "use strict";
+let inkr = 10,
+  dekr = 10;
+
+console.log(++inkr);
+console.log(dekr--);
+console.log(12 % 5);
+let isChecked = false;
+isClose = false;
+
+console.log(isChecked || isClose);
